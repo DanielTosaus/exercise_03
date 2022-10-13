aae6e09f4c5d8ead3e51d7835172d3214a5af06b
Daniel Tosaus <249935@vutbr.cz>
2022-10-13 09:50:58 +0200

    Added SelectionSort and RecursiveSelectionSort

--- task1.r
+++ task1.r
@@ -1,20 +1,58 @@
-
+# IndexOfMin
 IndexOfMin <- function(array, first, last){
   index <- first
-  for(k in first+1: last)
+  for(k in (first+1): last)
   {
 
     if(array[k] < array[index]){
       
       index <- k
-      print(index)
+      #print(index)
     }
     
   }
-  print(index)
+  index
 }
   
 array <- c(5, 2, 7, 4, 1)
 first <- 1
-last <- 4
+last <- 5
 IndexOfMin(array, first, last)
+
+# SelectionSort
+SelectionSort <- function(a, n){
+  last <- n-1
+  for(i in 1:last){
+    # find minimun in range i, n
+    sprintf("i: %d", i)
+    j <- IndexOfMin(a, i, n)
+    # swap indexes
+    tmp <- a[i]
+    a[i] <- a[j]
+    a[j] <- tmp
+  }
+  a
+}
+
+SelectionSort(array, 5)
+
+
+# Recursive sort
+RecursiveSelectionSort <- function(a, first, last){
+  if (first < last){
+    index <- IndexOfMin(a, first, last)
+    tmp <- a[first]
+    a[first] <- a[index]
+    a[index] <- tmp
+    first <- first + 1
+    a <- RecursiveSelectionSort(a, first, last)
+  }
+  a
+}
+
+
+RecursiveSelectionSort(array, 1, 5)
+
+
+
+
